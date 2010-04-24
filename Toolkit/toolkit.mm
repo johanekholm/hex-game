0783686531dd383c2dcc67b522639d49cca8b4d0
Johan Ekholm <johan.ekholm@gmail.com>
2010-04-25 01:46:50 +0200

    ...and also added the .mm versions of toolkit

---
+++ Toolkit/toolkit.mm
@@ -0,0 +1,20 @@
+/*
+ *  toolkit.c
+ *  igame
+ *
+ *  Created by Magnus Ahlberg on 2010-04-24.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "toolkit.h"
+
+GPoint GPointMake(GLfloat x, GLfloat y) {
+	GPoint point;
+	point.x = x;
+	point.y = y;
+	
+	return point;
+}
+
+
