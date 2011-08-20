61ae2c4d76f36dcb6ee9bdc63927576b85c57878
Magnus Ahlberg <magnus.ahlberg@specifik.se>
2011-08-20 18:17:12 +0200

    Add static member AssetManager to HexGame

--- Android/HexGame.java
+++ Android/HexGame.java
@@ -2,15 +2,18 @@ package com.hexgame.game;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 
 public class HexGame extends Activity {
     /** Called when the activity is first created. */
+	public static AssetManager assetManager;
 	public static Context context;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+		assetManager = getAssets();
 		context = getApplicationContext();
 		GameGLView view = new GameGLView(this);
    		view.setRenderer(new OpenGLRenderer());
