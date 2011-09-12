25af638148746f58a4e540e75f58c2bfc7534517
Johan Ekholm <johan.ekholm@gmail.com>
2011-09-12 22:51:03 +0200

    Added BaseUnitViewController

---
+++ Classes/BaseUnitViewController.h
@@ -0,0 +1,47 @@
+/*
+ *  BaseUnitViewController.h
+ *
+ *  Created by Johan Ekholm.
+ *  
+ *
+ */
+
+#ifndef BASEUNITVIEWCONTROLLER_H
+#define BASEUNITVIEWCONTROLLER_H
+
+#include "IObserver.h"
+#include "ViewController.h"
+#include <vector>
+
+class GameImage;
+class ShapeImage;
+struct ActionState;
+
+struct ActionView {
+    GPoint pos;
+    int actionId;
+    bool active;
+    ActionState* statePoint;
+};
+
+class BaseUnitViewController : public ViewController, public IObserver {
+    GameImage* _actionImage;
+    ShapeImage *_actionMarker;
+    ActionView* _selectedActionView;
+    std::vector<ActionView> _actionPoints;
+
+	void drawActions();	
+    ActionState* getTouchedActionState(GPoint point);
+    ActionView* getTouchedActionView(GPoint point);
+	void updateActions(std::vector<ActionState>& actionStates);
+	void updatePosition(const MPoint& pos);
+
+public:
+	virtual ~BaseUnitViewController();
+	BaseUnitViewController(GLfloat width, GLfloat height, int index);
+    void destroyed();
+	void drawGUI();
+    void setFocus(bool hasFocus);
+};
+
+#endif
