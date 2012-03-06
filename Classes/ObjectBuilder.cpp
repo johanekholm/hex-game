1976ca7f9cd6000374f3d055ffff4b138c383826
Johan Ekholm <johan.ekholm@gmail.com>
2012-03-06 22:38:27 +0100

    Cleanup in objectbuilder

--- Classes/ObjectBuilder.cpp
+++ Classes/ObjectBuilder.cpp
@@ -66,27 +66,8 @@ void ObjectBuilder::createMapObjectFromTemplate(const std::string& objectType, i
 
 UnitModel* ObjectBuilder::produceUnit(const std::string& unitClass, int owner, const MPoint& pos) {
     UnitModel* unit;
-    /*int hp, ap, power, skill, defense, image;
-    std::vector<int> actions;
-    if (unitClass == "swordsman") {
-        hp = 10; ap = 30; power = 3; skill = 2; defense = 3; image = 0;
-        actions.push_back(ActionNS::BACTION_MOVE); actions.push_back(ActionNS::BACTION_STRIKE);
-    } else if (unitClass == "soldier") {
-        hp = 1; ap = 30; power = 2; skill = 2; defense = 2; image = 1;
-        actions.push_back(ActionNS::BACTION_MOVE); actions.push_back(ActionNS::BACTION_STRIKE);
-    } else if (unitClass == "archer") {
-        hp = 4; ap = 30; power = 2; skill = 3; defense = 1; image = 3;
-        actions.push_back(ActionNS::BACTION_MOVE); actions.push_back(ActionNS::BACTION_FIRE);
-    } else if (unitClass == "channeler") {
-        hp = 4; ap = 50; power = 2; skill = 3; defense = 1; image = 2;
-        actions.push_back(ActionNS::BACTION_MOVE); actions.push_back(ActionNS::BACTION_BURN); actions.push_back(ActionNS::BACTION_HEAL);
-    } else {
-        return 0;
-    }*/
     
-    DEBUGLOG("Produce unit: %s", unitClass.c_str());
     unit = new UnitModel(unitClass, pos, owner); 
-	//new UnitModel(pos.x, pos.y, owner, hp, ap, power, skill, defense, actions, image);
     
     return unit;
 }
